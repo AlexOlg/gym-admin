0e3452029ab07433b801dea60b2386fda54b8714
David <davidabeddev@gmail.com>
2024-08-20 17:34:02 -0300

    sign in and sign up created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
 
 const App = () => {
   return (
-    <div>
-      GymApp
-      <h1>este es un nuevo titulo</h1>
+    <>
+    <div className="flex justify-center items-center min-h-screen bg-[#1F2023] dark:bg-gray-900">
+      <section className="flex flex-col justify-center items-center w-[500px] bg-slate-900 text-slate-50 max-w-1300 gap-2 rounded-2xl">
+        <h1 className="text-[45px]">🏋️‍♀️GymApp🏋️‍♀️</h1>
+        <label htmlFor="nombre-gym">¿Como se llama su gimnasio?</label>
+        <input type="text" name="nombre-gym" id="" className="rounded-[20px] bg-slate-700"/>
+        <label htmlFor="id-gym">Gimnasio ID</label>
+        <input type="text" name="id-gym" id="" className="rounded-[20px] bg-slate-700"/>
+        <label htmlFor="nombre">Email</label>
+        <input type="email" name="email" id="" className="rounded-[20px] bg-slate-700"/>
+        <label htmlFor="password">Contraseña</label>
+        <input type="password" name="password" id="" className="rounded-[20px] bg-slate-700"/>
+        <button type="submit" className="bg-[#CCDE5F]	text-[black] rounded-[10px]	px-6 m-3">Registrarse</button>
+      </section>
     </div>
+    </>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import { SignUp } from "./components/pages/singup/SignUp.tsx";
+import SignIn from "./components/pages/signIn/ SignIn.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter >
+    <Routes>
+      <Route index element={<App/>}/>
+      {/* <Route path="/singin" element={<SingIn/>}/> */}
+      <Route path="/signup/" element={<SignUp/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+
+
+    </Routes>
+  </BrowserRouter>
+);
