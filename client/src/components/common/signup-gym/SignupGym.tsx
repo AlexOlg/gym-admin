f990b334e91f229cfdc816aa261f9e58ae2c9dde
David <davidabeddev@gmail.com>
2024-08-27 21:01:15 -0300

    sign in and sign up fixed

---
+++ client/src/components/common/signup-gym/SignupGym.tsx
@@ -0,0 +1,139 @@
+import React from 'react'
+
+const SignupGym = () => {
+  return (
+
+
+      <div className="w-96  bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-6 space-y-2 md:space-y-3 ">
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
+            Create new Gym 
+          </h1>
+          <form className="space-y-4 md:space-y-6" action="#">
+            <div>
+              <label
+                htmlFor="gym_id"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                GYM ID
+              </label>
+              <input
+                type="text"
+                name="gym_id"
+                id="gym_id"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="****"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="gym_name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your Gym Name
+              </label>
+              <input
+                type="text"
+                name="gym_name"
+                id="gym_name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="&quot;Gym World's&quot;"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="name@company.com"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••••"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="confirm-password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Confirm password
+              </label>
+              <input
+                type="password"
+                name="confirm-password"
+                id="confirm-password"
+                placeholder="••••••••"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+            </div>
+            <div className="flex items-start">
+              <div className="flex items-center h-5">
+                <input
+                  id="terms"
+                  aria-describedby="terms"
+                  type="checkbox"
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                  required
+                />
+              </div>
+              <div className="ml-3 text-sm">
+                <label
+                  htmlFor="terms"
+                  className="font-light text-gray-500 dark:text-gray-300"
+                >
+                  I accept the{" "}
+                  <a
+                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    href="#"
+                  >
+                    Terms and Conditions
+                  </a>
+                </label>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="w-full text-white bg-blue-900 hover:bg-blue-800 hover:duration-150  font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
+            >
+              Create an account
+            </button>
+            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              Already have an account?{" "}
+              <a
+                href="/signin"
+                className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+              >
+                Login here
+              </a>
+            </p>
+          </form>
+        </div>
+      </div>
+  )
+}
+
+export default SignupGym
